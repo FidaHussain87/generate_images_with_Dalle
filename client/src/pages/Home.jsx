49af35e8f1a0735a06b79471daaa2b0fa4db6e12
FidaHussain87 <36170696+FidaHussain87@users.noreply.github.com>
2023-05-21 12:16:28 +0200

    url added for frontend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true)
       try {
-        const response = await fetch('https://dalle2-0-ss6m.onrender.com/api/v1/post', {
+        const response = await fetch('https://generate-images-with-dalle.vercel.app/api/v1/post', {
           method: 'GET',
           headers: { 'Content-Type': 'application/json' },
         })
